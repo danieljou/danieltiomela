174121f5619c05507bacca43797a8e1351ec481c
danieljou <danielsitepro@gmail.com>
2024-01-30 14:22:36 +0100

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,7 @@ function App() {
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/resume" element={<Resume />} />
+					<Route path="/projects" element={<Projects />} />
 					<Route path="*" element={<Navigate to="/" />} />
 				</Routes>
 				<Footer />
